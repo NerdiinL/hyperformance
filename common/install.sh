d29463e0de8819cc482f19b44d0ae39662503886
NerdiinL <67336904+NerdiinL@users.noreply.github.com>
2021-02-02 14:48:07 -0300

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -1,1 +1,21 @@
-#
+
+print_modname() {
+sleep 0.9
+  ui_print "_______________________________"
+sleep 0.9
+  ui_print "          Apolon  V3.2 "
+sleep 0.9
+  ui_print "        Dev Haxis :::   "
+sleep 0.9
+  ui_print "   Dev Official @asli_immi   "
+sleep 0.9
+  ui_print "_______________________________"
+sleep 0.9
+  ui_print "💉APLICANDO A VACINA...."
+sleep 9.0
+  ui_print "Tudo Certo por equanto.."
+sleep 2.4
+  ui_print "oh oh o que é isto !!!!!"
+sleep 5.7
+  ui_print "Perfil gaming aplicado com sucesso !"
+  }
