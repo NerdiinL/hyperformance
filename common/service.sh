e4079116342c8f1af4710233eb2bebcc33710c1c
NerdiinL <67336904+NerdiinL@users.noreply.github.com>
2021-02-02 16:57:10 -0300

    Create service.sh

---
+++ common/service.sh
@@ -0,0 +1,10 @@
+#========================================
+# Hyperformance
+# Developer : Haxis & Nerdiin
+#========================================
+#!/system/bin/sh
+MODDIR=${0%/*}
+
+$MODDIR/system/bin/extreme 2>/dev/null
+$MODDIR/system/etc/init.d/tweaks 2>/dev/null
+$MODPATH/system/etc/init.d/touch 2>/dev/null
